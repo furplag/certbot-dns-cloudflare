ae37717dd93e9d493adc5727612c21cc6274292f
furplag <furplag@users.noreply.github.com>
2017-09-06 16:59:53 +0900

    Update authenticator.sh

--- authenticator.sh
+++ authenticator.sh
@@ -1,27 +1,60 @@
 #!/bin/bash
+set -ue -o pipefail
+export LC_ALL=C
+###
+# authenticator.sh
+# https://github.com/furplag/certbot-dns
+# Copyright 2017 furplag
+# Licensed under Apache 2.0 (https://github.com/furplag/certbot-dns/blob/master/LICENSE)
+
+# You must specify these variables in /etc/sysconfig/certbot.
+# - CF_AUTH_KEY : Cloudflare API param: "X-Auth-Key"
+# - CF_AUTH_EMAIL : Cloudflare API param: "X-Auth-Email"
+
+# Optional variables
+declare -r BASE_URL=https://api.cloudflare.com/client/v4/zones
+CERTBOT_DOMAIN=${CERTBOT_DOMAIN:-NOPE}
+CERTBOT_VALIDATION=${CERTBOT_VALIDATION:-NOPE}
 
 # Strip only the top domain to get the zone id
 DOMAIN=$(expr match "$CERTBOT_DOMAIN" '.*\.\(.*\..*\)')
 
-# Get the Cloudflare zone id
-ZONE_EXTRA_PARAMS="status=active&page=1&per_page=20&order=status&direction=desc&match=all"
-ZONE_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$DOMAIN&$ZONE_EXTRA_PARAMS" \
-     -H     "X-Auth-Email: $EMAIL" \
-     -H     "X-Auth-Key: $API_KEY" \
-     -H     "Content-Type: application/json" | python -c "import sys,json;print(json.load(sys.stdin)['result'][0]['id'])")
+ZONE_ID=$(curl -s -X GET "${BASE_URL}?name=${DOMAIN}&status=active&per_page=1" \
+  -H  "X-Auth-Key:${CF_AUTH_KEY:-N/A}" \
+  -H  "X-Auth-Email:${CF_AUTH_EMAIL:-${EMAIL}}" \
+  -H  "Content-Type: application/json" \
+  | python -c "import sys;import json;data=json.load(sys.stdin);print(data['result'][0]['id']) if data['success'] and data['result_info']['count'] > 0 else False;")
 
-# Create TXT record
-CREATE_DOMAIN="_acme-challenge.$CERTBOT_DOMAIN"
-RECORD_ID=$(curl -s -X POST "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records" \
-     -H     "X-Auth-Email: $EMAIL" \
-     -H     "X-Auth-Key: $API_KEY" \
-     -H     "Content-Type: application/json" \
-     --data '{"type":"TXT","name":"'"$CREATE_DOMAIN"'","content":"'"$CERTBOT_VALIDATION"'","ttl":120}' \
-             | python -c "import sys,json;print(json.load(sys.stdin)['result']['id'])")
-# Save info for cleanup
-if [ ! -d /tmp/CERTBOT_$CERTBOT_DOMAIN ];then
-        mkdir -m 0700 /tmp/CERTBOT_$CERTBOT_DOMAIN
+if [ $(echo $ZONE_ID) = "False" ]; then exit 1; fi
+
+RECORD_ID=$(curl -s -X GET "${BASE_URL}/${ZONE_ID}/dns_records?type=TXT&name=_acme-challenge.${$CERTBOT_DOMAIN}&per_page=1" \
+  -H  "X-Auth-Key:${CF_AUTH_KEY:-N/A}" \
+  -H  "X-Auth-Email:${CF_AUTH_EMAIL:-${EMAIL}}" \
+  -H  "Content-Type: application/json" \
+  | python -c "import sys;import json;data=json.load(sys.stdin);print(data['result'][0]['id']) if data['success'] and data['result_info']['count'] > 0 else False;")
+
+if [ $(echo $RECORD_ID) = "False" ]; then
+  RECORD_ID=$(curl -s -X POST "${BASE_URL}/${ZONE_ID}/dns_records" \
+  -H  "X-Auth-Key:${CF_AUTH_KEY:-N/A}" \
+  -H  "X-Auth-Email:${CF_AUTH_EMAIL:-${EMAIL}}" \
+  -H  "Content-Type: application/json" \
+  --data '{"type":"TXT","name":"'"_acme-challenge.${$CERTBOT_DOMAIN}"'","content":"'"$CERTBOT_VALIDATION"'"}' \
+  | python -c "import sys;import json;data=json.load(sys.stdin);print(data['result'][0]['id']) if data['success'] and data['result_info']['count'] > 0 else False;")
+else
+  RECORD_ID=$(curl -s -X POST "${BASE_URL}/${ZONE_ID}/dns_records/${RECORD_ID}" \
+  -H  "X-Auth-Key:${CF_AUTH_KEY:-N/A}" \
+  -H  "X-Auth-Email:${CF_AUTH_EMAIL:-${EMAIL}}" \
+  -H  "Content-Type: application/json" \
+  --data '{"type":"TXT","name":"'"_acme-challenge.${$CERTBOT_DOMAIN}"'","content":"'"$CERTBOT_VALIDATION"'"}' \
+  | python -c "import sys;import json;data=json.load(sys.stdin);print(data['result'][0]['id']) if data['success'] and data['result_info']['count'] > 0 else False;")
 fi
+
+if [ $(echo $RECORD_ID) = "False" ]; then exit 1; fi
+
+if [ ! -d /tmp/CERTBOT_$CERTBOT_DOMAIN ];then
+  mkdir -m 0700 /tmp/CERTBOT_$CERTBOT_DOMAIN
+fi
+
 echo $ZONE_ID > /tmp/CERTBOT_$CERTBOT_DOMAIN/ZONE_ID
 echo $RECORD_ID > /tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID
 
