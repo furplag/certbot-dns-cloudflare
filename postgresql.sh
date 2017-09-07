5400d4a3fff0cc72e2e867723cc4ffca27fc3d19
furplag <furplag@users.noreply.github.com>
2017-09-07 15:01:04 +0900

    Update postgresql.sh

--- postgresql.sh
+++ postgresql.sh
@@ -11,25 +11,26 @@ export LC_ALL=C
 
 ###
 # variables
-# - PG_DATA : path to postgresql data directory .
+# - PGDATA : path to postgresql data directory .
 # - PG_SERVICE : the name of postgresql service .
 # important : You should write those in /etc/sysconfig/certbot .
 
-if [ ! -z "${PG_DATA}" ]; then exit 0; fi
-if [ ! -z "${PG_SERVICE}" ]; then exit 0; fi
-if [ ! -z "${CERTBOT_DOMAIN}" ]; then echo "undefined: CERTBOT_DOMAIN ."; exit 0; fi
+if [ -z "${PGDATA}" ]; then echo "undefined: PG_DATA ."; exit 0; fi
+if [ -z "${PG_SERVICE}" ]; then echo "undefined: PG_SERVICE ."; exit 0; fi
+if [ -z "${CERTBOT_DOMAIN}" ]; then echo "undefined: CERTBOT_DOMAIN ."; exit 0; fi
 
 if [ -f "/etc/letsencrypt/live/${CERTBOT_DOMAIN}/privkey.pem" ]; then
-  rm -rf ${PG_DATA}/privkey.pem && \
+  rm -rf ${PGDATA}/privkey.pem
   cp -p "/etc/letsencrypt/live/${CERTBOT_DOMAIN}/privkey.pem" "${PG_DATA}/." && \
   chown postgres:postgres ${PG_DATA}/privkey.pem && \
-  chmod 0600 ${PG_DATA}/privkey.pem;
-fi
-if [ -f "/etc/letsencrypt/live/${CERTBOT_DOMAIN}/fullchain.pem" ]; then
-  rm -rf ${PG_DATA}/fullchain.pem && \
-  cp -p "/etc/letsencrypt/live/${CERTBOT_DOMAIN}/fullchain.pem" "${PG_DATA}/." && \
-  chown postgres:postgres ${PG_DATA}/fullchain.pem && \
-  chmod 0600 ${PG_DATA}/fullchain.pem;
+  chmod 0600 ${PG_DATA}/privkey.pem
 fi
 
-/usr/bin/systemctl restart postgresql-9.6.service
+if [ -f "/etc/letsencrypt/live/${CERTBOT_DOMAIN}/fullchain.pem" ]; then
+  rm -rf ${PGDATA}/fullchain.pem
+  cp -p "/etc/letsencrypt/live/${CERTBOT_DOMAIN}/fullchain.pem" "${PG_DATA}/." && \
+  chown postgres:postgres ${PG_DATA}/fullchain.pem && \
+  chmod 0600 ${PG_DATA}/fullchain.pem
+fi
+
+/usr/bin/systemctl restart "${PG_SERVICE}.service"
