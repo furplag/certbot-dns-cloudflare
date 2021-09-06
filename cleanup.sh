3fad65f8ce9306c8cdd02a7d0b35c8987afbe635
furplag <furplag@users.noreply.github.com>
2021-09-06 17:34:32 +0900

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -26,8 +26,8 @@ declare -r _auth_key=${CLOUDFLARE_AUTH_KEY:-}
 declare -r _email=${CLOUDFLARE_AUTH_EMAIL:-}
 
 declare -r _tld=$(expr match "${CERTBOT_DOMAIN}" '.*\.\(.*\..*\)')
-declare -r _domain_root=${_tld:-$CERTBOT_DOMAIN}
-declare -r _domain=$(echo "${_domain_root}" | sed -e "s/^\*\.//")
+declare -r _domain_root=${_tld:-$CERTBOT_DOMAIN:-}
+declare -r _domain=$(echo "${CERTBOT_DOMAIN:-}" | sed -e "s/^\*\.//")
 
 declare -r _logDir=/var/log/letsencrypt
 declare -r _datetime=`date +"%Y%m%d%H%M%S"`
